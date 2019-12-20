d918dae125cf28ce18bd281ffa1d7e4ca1c5e313
Qinyixiong <qinyixiong@pansoft.com>
2019-12-20 17:25:07 +0800

    通用es接口

---
+++ src/ElasticsearchAPI/ElasticSearchDB.java
@@ -0,0 +1,12 @@
+package ElasticsearchAPI;
+
+/**
+ * @author: 覃义雄
+ * @dateTime: 2019-12-20 15:30
+ * @project_Name: PersonalPractice
+ * @Name: ElasticSearchDB
+ * @Describe：
+ */
+public class ElasticSearchDB {
+
+}
